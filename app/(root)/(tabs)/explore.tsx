5bcfe25ba6023cb09f36bc0e7e0bb24c4b2864d9
Aliaksandr Horbach <horbachalex91@gmail.com>
2025-01-05 14:48:51 +0100

    init: start project

---
+++ app/(root)/(tabs)/explore.tsx
@@ -0,0 +1,10 @@
+import {View, Text} from "react-native";
+
+const Explore = () =>{
+    return (
+        <View>sss
+            <Text>Explore</Text>
+        </View>
+    )
+}
+export default Explore
