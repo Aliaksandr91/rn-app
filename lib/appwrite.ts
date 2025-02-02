8b580037cb7628d9a8d88fdfebd0bf08daf8714f
Aliaksandr Horbach <horbachalex91@gmail.com>
2025-02-02 13:51:27 +0100

    feat: appwrite DB setup

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -5,10 +5,16 @@ import {
 } from "react-native-appwrite";
 import * as Linking from "expo-linking";
 import {openAuthSessionAsync} from "expo-web-browser";
+
 export const config = {
     platform: "com.jsm.restate",
     endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
-    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+    galleriesCollectionId: process.env.EXPO_PUBLIC_APPWRITE_GALLERIES_COLLECTION_ID,
+    reviewsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_REVIEWS_COLLECTION_ID,
+    agentsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_AGENTS_COLLECTION_ID,
+    propertiesCollectionId: process.env.EXPO_PUBLIC_APPWRITE_PROPERTIES_COLLECTION_ID,
 }
 export const client = new Client();
 client
@@ -17,6 +23,7 @@ client
     .setPlatform(config.platform!);
 export const avatar = new Avatars(client);
 export const account = new Account(client);
+
 export async function login() {
     try {
         const redirectUri = Linking.createURL("/");
